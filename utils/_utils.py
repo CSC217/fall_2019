367825f3a5829cfff201faf79434e885ec2b9631
Evan Agovino <evanagovino@stsh-01407-evan.agovino-mbpr13t>
2019-11-20 18:11:40 -0500

    uploaded new materials

--- utils/_utils.py
+++ utils/_utils.py
@@ -1,6 +1,18 @@
 import numpy as np
 import pandas as pd
 
+def assert_correct_type(type):
+    def decorator(f):
+        def new_f(*args, **kwargs):
+            newargs = []
+            for a in args:
+                newargs.append(type(a))       
+            return f(*newargs, **kwargs)
+        return new_f
+    return decorator
+
+
+@assert_correct_type(np.array)
 def evaluate_linear_relationship(a, b):
     """
 
@@ -27,16 +39,7 @@ def evaluate_linear_relationship(a, b):
     residuals: numpy array
         The absolute difference between the predicted values and actual values
         of b
-
-
-    slope:
     """
-    assert (type(a) == list) or (type(a) == np.array) or (type(a) == pd.Series)
-    assert (type(b) == list) or (type(b) == np.array) or (type(b) == pd.Series)
-    if type(a) != np.array:
-        a = np.array(a)
-    if type(b) != np.array:
-        b = np.array(b)
     slope = np.cov(a, b, bias=True)[0][1] / np.var(a)
     intercept = np.mean(b) - (slope * np.mean(a))
     predictions = (slope * a) + intercept
@@ -100,3 +103,69 @@ def bootstrap_mean_diff(a, b, number_of_simulations=10000, seed_value=42):
         new_b = total[len(a):]
         diff.append(np.mean(new_a) - np.mean(new_b))
     return diff
+
+@assert_correct_type(np.array)
+def get_rmse(predictions, actual_values):
+    """
+
+    Return the Root Mean Squared Error for a set of predicted and actual values.
+
+    Parameters
+    ----------
+
+    predictions: list, numpy array or Pandas series
+        Predicted values.
+    actual_values: list, numpy array or Pandas series
+        Actual values.
+
+    Returns
+    -------
+
+    rmse: float
+        The Root Mean Squared Error between your predicted and actual values.
+    """
+
+    return np.sqrt(((predictions - actual_values) ** 2).mean())
+
+def get_r2(predictions, actual_values):
+    """
+
+    Return the R2 valuefor a set of predicted and actual values.
+
+    Parameters
+    ----------
+
+    predictions: list, numpy array or Pandas series
+        Predicted values.
+    actual_values: list, numpy array or Pandas series
+        Actual values.
+
+    Returns
+    -------
+
+    rmse: float
+        The R2 value between your predicted and actual values.
+    """
+    return np.var(predictions) / np.var(actual_values)
+
+@assert_correct_type(np.array)
+def get_train_test(x, y, test_size=0.2):
+    """
+
+    Split a distribution into a training and test set.
+
+    Parameters
+    ----------
+
+    x: list, numpy array or Pandas series
+        Predictor variable.
+    test_size: float, optional
+        the percentage of your data you want to be in the test set
+
+
+    """
+    np.random.seed(42)
+    test_length = int(len(x) * test_size)
+    test_index = np.random.choice(range(len(x)), size=test_length)
+    train_index = np.array([i for i in range(len(x)) if i not in test_index])
+    return np.array(x[train_index]), np.array(x[test_index]), np.array(y[train_index]), np.array(y[test_index])
---
+++ week11-regression_ii/README.md
@@ -0,0 +1,55 @@
+# Week Eleven: More Regression
+
+This week, we'll continue to look at the different applications of regression.
+
+## Notes
+
+If you've downloaded the class repo to your local computer, be sure to go into your folder and run 'git pull' to download any new files.
+
+## Upcoming Dates  
+
+**November 27th**  
+*No Class*
+
+**December 11th**  
+*Project Due*
+
+
+## Lecture
+
+See [here](https://github.com/CSC217/fall_2019/blob/master/week11-regression_ii/Week_Eleven_More_Regression.ipynb) for this week's lecture.
+
+## Homework
+
+See [here](https://github.com/CSC217/fall_2019/blob/master/week11-regression_ii/Homework_Eleven.ipynb) for this week's homework. Homework is due December 4th at 6:30 PM sharp. Anything submitted after that will receive a 0.
+
+## Classwork
+
+See [here](https://github.com/CSC217/fall_2019/blob/master/week11-regression_ii/Regression_Workbook.ipynb) for this week's classwork.
+
+## Extra Credit Assignments
+
+Please note that either of the extra credit assignments from Week 9 and Week 10 are eligible to be submitted until **November 27th at 6:30 PM**. Please reference the README.md files from those repos to see what the assignment entails.
+
+Each submission can get you up to **5 extra points** on your midterm.
+
+They are due **November 27th at 6:30 PM** sharp - nothing submitted after that will be accepted. Please submit it to my email with the subject 'Extra Credit Assignment \#3'
+
+## Readings
+
+#### [Linear Regression](https://ocw.mit.edu/courses/mathematics/18-05-introduction-to-probability-and-statistics-spring-2014/readings/MIT18_05S14_Reading25.pdf)
+
+Some basics on linear regression.
+
+#### [Linear Regression Video](https://www.youtube.com/watch?v=CtKeHnfK5uA)  
+This video, if you can handle the French accent of the narrator :), is another good resource for understanding the basics of linear regression.
+
+#### [Interpreting Log Transformations in a Linear Model](https://data.library.virginia.edu/interpreting-log-transformations-in-a-linear-model/)
+
+This page has a helpful for interpreting how to interpret a linear regression model when either the predictor or response variable, or both, are log transformed.
+
+#### [Binary Logistic Regression With a Single Categorical Predictor](https://newonlinecourses.science.psu.edu/stat504/node/150/)
+
+#### [Intro to Multivariate Regression Models](https://towardsdatascience.com/data-science-simplified-part-5-multivariate-regression-models-7684b0489015)
+
+#### [Intro to Polynomial Regression](https://towardsdatascience.com/polynomial-regression-bbe8b9d97491)
